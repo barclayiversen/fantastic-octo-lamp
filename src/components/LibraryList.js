3e9f761571fb1053e96706932a42a00e8cbe4d62
Barclay Iversen <biversen@biversen-macbookpro.roam.corp.google.com>
2017-10-06 14:13:37 -0700

    lecture 92 section 13

---
+++ src/components/LibraryList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { ListView } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+	componentWillMount() {
+		const ds = new ListView.DataSource({
+			rowHasChanged: () => (r1, r2) => r1 !== r2
+		});
+		
+		this.dataSource = ds.cloneWithrows(this.props.libraries);
+	}
+	
+	render() {
+		console.log(this.props);
+		return (
+			<ListView
+				dataSource={this.dataSource}
+			/>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
